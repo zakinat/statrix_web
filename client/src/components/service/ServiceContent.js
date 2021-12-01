049e6148c6431e94c66abce50283d7e368283d89
zaki <natourzaki@gmail.com>
2021-12-02 04:00:42 +0700

    features added dots array to the pics

--- client/src/components/service/ServiceContent.js
+++ client/src/components/service/ServiceContent.js
@@ -2,27 +2,30 @@ import React from 'react'
 
 const ServiceContent = ({title,spcWord,text,services,index}) => {
     return (
-        <div className='serviceContent'>
-            <div className='serviceContent__title'>
-                <soan>{index}</soan>
+        <div className='service__content-info'>
+            <div className='service__content-info-title'>
+                <span className='service__content-info-title-index'>{index}</span>
                 <h2>{title}</h2>
                 {spcWord.length>0 && <span className='title__badge'>{spcWord}</span>}
             </div>
-            <div className='serviceContent__text' >
+            <div className='service__content-info-text' >
                 <p>{text}</p>
             </div>
-            <div className='serviceContent__services'>
+            <div className='service__content-info-services'>
                 {services.map((service,index)=>
-                <p>{service}</p>)}
+                <div className='service__content-info-services-service'>
+                    <span></span>
+                    <p>{service}</p>
+                </div>)}
             </div>
         </div>
     )
 }
 
-const ServiceImage=({img})=>{
+const ServiceImage=({img,imgSide})=>{
     return(
-            <div className='ServiceWildberries__img'>
-                <img src={img}/>
+            <div className={`service__img ${imgSide}`}>
+                <img src={img} alt='service image1'/>
             </div>
     )
 }
--- client/src/components/service/ServiceWildberries.js
+++ client/src/components/service/ServiceWildberries.js
@@ -3,13 +3,13 @@ import {ServiceContent, ServiceImage } from './ServiceContent'
 
 
 
-const ServiceContenSideWildberries=({title,spcWord,text,services,index})=>{
+const ServiceContentWilberries=({title,spcWord,text,services,index})=>{
     return(
-            <div className='ServiceWildberries__content'>
+            <div className='service__content serviceWildberries__content'>
                 <ServiceContent title ={title} spcWord ={spcWord} text ={text} services ={services} index ={index} />
                 <div className='ServiceWildberries__content-askDemo'>
                         <button type="button"  >Попробовать 7 дней бесплатно</button>
-                        <button type="button" >Запросить демо-режим</button>
+                        <button type="button" >Узнать больше</button>
                 </div>
             </div>
     )
@@ -19,18 +19,18 @@ const ServiceContenSideWildberries=({title,spcWord,text,services,index})=>{
 
 const ServiceWildberries = ({title,spcWord,text,services,img,index}) => {
     return (
-        <div className='ServiceWildberries'>
+        <div className='service servic__Wildberries'>
             {
-                (index%2==0)? (
+                (index%2===0)? (
                     <>
-                        <ServiceImage img={img}/>
-                        <ServiceContenSideWildberries title={title} spcWord={spcWord} text={text} services={services} index={index+1} />
+                        <ServiceImage img={img} imgSide={'leftImage'}/>
+                        <ServiceContentWilberries title={title} spcWord={spcWord} text={text} services={services} index={index+1} />
                     </>
                 ): 
                 (
                     <>
-                        <ServiceContenSideWildberries title={title} spcWord={spcWord} text={text} services={services} index={index+1} />
-                        <ServiceImage img={img}/>
+                        <ServiceContentWilberries title={title} spcWord={spcWord} text={text} services={services} index={index+1} />
+                        <ServiceImage img={img} imgSide={'rightImage'}/>
                     </>
                 )
             }
